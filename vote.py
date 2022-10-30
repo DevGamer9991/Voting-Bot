2402d9177a0c6d7c81ea1a460638ca0088c6dff7
DevGamer9991 <pathornton114@gmail.com>
2022-10-30 16:33:44 -0500

    Update

--- vote.py
+++ vote.py
@@ -6,28 +6,22 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 from threading import Thread
 
-firstClr = False
-
 options = Options()
 
 options.add_argument('--log-level=3')
-options.add_argument("--window-size=640,480")
-# options.add_argument('headless')
+options.add_argument("--window-size=1920,1080")
+options.add_argument('headless')
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 counter = 1
 
 url = "https://okc.vypeok.com/2022/10/25/vote-now-okc-area-private-schools-miss-volleyball-presented-by-community-builders-poll-ends-11-1/"
 
-def bot(counter, firstClr, url):
+def bot(counter, url):
     while True:
         driver = webdriver.Chrome("./chromedriver.exe", chrome_options=options, service_log_path='NULL')
 
-        if (firstClr == False):
-            os.system("cls")
-            firstClr = True
-
-        print(f"Start")
+        print(f"Started New Vote")
         counter += 1
         driver.get(url)
 
@@ -39,7 +33,7 @@ def bot(counter, firstClr, url):
             driver.implicitly_wait(.50)
             voteButton.click()
 
-            voteTitle = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "question-top-11227721")))
+            voteTitle = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.ID, "question-top-11227721")))
 
             voterWrapper = driver.find_element(By.ID, "pds-results")
 
@@ -51,7 +45,7 @@ def bot(counter, firstClr, url):
                 name = voter.find_element(By.TAG_NAME, "label").find_element(By.TAG_NAME, "span").text
                 if (name == "Natalie French – Christian Heritage"):
 
-                    votes = voter.find_element(By.XPATH, "//label/span/span[@class='pds-feedback-votes']").text
+                    votes = voter.find_element(By.TAG_NAME, "label").find_element(By.CLASS_NAME, "pds-feedback-result").find_element(By.CLASS_NAME, "pds-feedback-votes").text
                     votes = votes.split("(")[1]
                     votes = votes.split(")")[0]
                     votes = votes.split(" votes")[0]
@@ -71,23 +65,40 @@ def bot(counter, firstClr, url):
                     break
                 else:
                     place += 1
+                if (name == "Gracie Maschmeier – Crossing Christian"):
+                    votes = voter.find_element(By.TAG_NAME, "label").find_element(By.CLASS_NAME, "pds-feedback-result").find_element(By.CLASS_NAME, "pds-feedback-votes").text
+                    votes = votes.split("(")[1]
+                    votes = votes.split(")")[0]
+                    votes = votes.split(" votes")[0]
 
-            driver.close()
+                    print(f"Gracie Has { votes } votes!")
+
+            driver.quit()
         except Exception as e:
             print("Error Occured!")
-            driver.kill()
+            driver.quit()
             continue
 
-t1 = Thread(target=bot, args=(counter, firstClr, url))
-t2 = Thread(target=bot, args=(counter, firstClr, url))
-t3 = Thread(target=bot, args=(counter, firstClr, url))
-t4 = Thread(target=bot, args=(counter, firstClr, url))
-t5 = Thread(target=bot, args=(counter, firstClr, url))
-t6 = Thread(target=bot, args=(counter, firstClr, url))
-t7 = Thread(target=bot, args=(counter, firstClr, url))
-t8 = Thread(target=bot, args=(counter, firstClr, url))
-t9 = Thread(target=bot, args=(counter, firstClr, url))
-t10 = Thread(target=bot, args=(counter, firstClr, url))
+t1 = Thread(target=bot, args=(counter, url))
+t2 = Thread(target=bot, args=(counter, url))
+t3 = Thread(target=bot, args=(counter, url))
+t4 = Thread(target=bot, args=(counter, url))
+t5 = Thread(target=bot, args=(counter, url))
+t6 = Thread(target=bot, args=(counter, url))
+t7 = Thread(target=bot, args=(counter, url))
+t8 = Thread(target=bot, args=(counter, url))
+t9 = Thread(target=bot, args=(counter, url))
+t10 = Thread(target=bot, args=(counter, url))
+t11 = Thread(target=bot, args=(counter, url))
+t12 = Thread(target=bot, args=(counter, url))
+t13 = Thread(target=bot, args=(counter, url))
+t14 = Thread(target=bot, args=(counter, url))
+t15 = Thread(target=bot, args=(counter, url))
+t16 = Thread(target=bot, args=(counter, url))
+t17 = Thread(target=bot, args=(counter, url))
+t18 = Thread(target=bot, args=(counter, url))
+t19 = Thread(target=bot, args=(counter, url))
+t20 = Thread(target=bot, args=(counter, url))
 
 t1.start()
 t2.start()
@@ -99,3 +110,13 @@ t7.start()
 t8.start()
 t9.start()
 t10.start()
+t11.start()
+t12.start()
+t13.start()
+t14.start()
+t15.start()
+t16.start()
+t17.start()
+t18.start()
+t19.start()
+t20.start()
