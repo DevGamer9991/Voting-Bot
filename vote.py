79d3f7052a227b7d1eefd876bfd635e0895c50c1
DevGamer9991 <pathornton114@gmail.com>
2022-10-30 15:23:49 -0500

    Added Multithreading and Multi vote

--- vote.py
+++ vote.py
@@ -4,74 +4,98 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
+from threading import Thread
 
 firstClr = False
 
 options = Options()
 
 options.add_argument('--log-level=3')
-options.add_argument("--window-size=1920,1080")
-options.add_argument('headless')
+options.add_argument("--window-size=640,480")
+# options.add_argument('headless')
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 counter = 1
 
 url = "https://okc.vypeok.com/2022/10/25/vote-now-okc-area-private-schools-miss-volleyball-presented-by-community-builders-poll-ends-11-1/"
 
-while True:
-    driver = webdriver.Chrome("./chromedriver.exe", chrome_options=options, service_log_path='NULL')
-    
-    if (firstClr == False):
-        os.system("cls")
-        firstClr = True
+def bot(counter, firstClr, url):
+    while True:
+        driver = webdriver.Chrome("./chromedriver.exe", chrome_options=options, service_log_path='NULL')
 
-    print(f"\nOpening URL For the {counter} time.\n")
-    counter += 1
-    driver.get(url)
+        if (firstClr == False):
+            os.system("cls")
+            firstClr = True
 
-    try: 
-        inputEl = driver.find_element(By.ID, "PDI_answer51314002")
-        voteButton = driver.find_element(By.ID, "pd-vote-button11227721")
+        print(f"Start")
+        counter += 1
+        driver.get(url)
 
-        inputEl.click()
-        driver.implicitly_wait(0.25)
-        voteButton.click()
+        try: 
+            inputEl = driver.find_element(By.ID, "PDI_answer51314002")
+            voteButton = driver.find_element(By.ID, "pd-vote-button11227721")
 
-        voteTitle = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "question-top-11227721")))
+            inputEl.click()
+            driver.implicitly_wait(.50)
+            voteButton.click()
 
-        voterWrapper = driver.find_element(By.ID, "pds-results")
+            voteTitle = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "question-top-11227721")))
 
-        voters = voterWrapper.find_elements(By.TAG_NAME, "li")
+            voterWrapper = driver.find_element(By.ID, "pds-results")
 
-        votes = 0
-        place = 1
-        for voter in voters:
-            name = voter.find_element(By.TAG_NAME, "label").find_element(By.TAG_NAME, "span").text
-            if (name == "Natalie French – Christian Heritage"):
+            voters = voterWrapper.find_elements(By.TAG_NAME, "li")
 
-                votes = voter.find_element(By.XPATH, "//label/span/span[@class='pds-feedback-votes']").text
-                votes = votes.split("(")[1]
-                votes = votes.split(")")[0]
-                votes = votes.split(" votes")[0]
+            votes = 0
+            place = 1
+            for voter in voters:
+                name = voter.find_element(By.TAG_NAME, "label").find_element(By.TAG_NAME, "span").text
+                if (name == "Natalie French – Christian Heritage"):
 
-                placeString = ""
-                if (place == 1):
-                    placeString = "1st"
-                elif (place == 2):
-                    placeString = "2nd"
-                elif (place == 3):
-                    placeString = "3rd"
+                    votes = voter.find_element(By.XPATH, "//label/span/span[@class='pds-feedback-votes']").text
+                    votes = votes.split("(")[1]
+                    votes = votes.split(")")[0]
+                    votes = votes.split(" votes")[0]
+
+                    placeString = ""
+                    if (place == 1):
+                        placeString = "1st"
+                    elif (place == 2):
+                        placeString = "2nd"
+                    elif (place == 3):
+                        placeString = "3rd"
+                    else:
+                        placeString = place + "th"
+
+                    print(f"Natalie Has { votes } votes! And She is in { placeString } place!")
+                    
+                    break
                 else:
-                    placeString = place + "th"
+                    place += 1
 
-                print(f"Natalie Has { votes } votes! And She is in { placeString } place!")
-                
-                break
-            else:
-                place += 1
+            driver.close()
+        except Exception as e:
+            print("Error Occured!")
+            driver.kill()
+            continue
 
-        driver.close()
-    except Exception as e:
-        print("Error Occured!")
-        driver.close()
-        continue
+t1 = Thread(target=bot, args=(counter, firstClr, url))
+t2 = Thread(target=bot, args=(counter, firstClr, url))
+t3 = Thread(target=bot, args=(counter, firstClr, url))
+t4 = Thread(target=bot, args=(counter, firstClr, url))
+t5 = Thread(target=bot, args=(counter, firstClr, url))
+t6 = Thread(target=bot, args=(counter, firstClr, url))
+t7 = Thread(target=bot, args=(counter, firstClr, url))
+t8 = Thread(target=bot, args=(counter, firstClr, url))
+t9 = Thread(target=bot, args=(counter, firstClr, url))
+t10 = Thread(target=bot, args=(counter, firstClr, url))
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+t6.start()
+t7.start()
+t8.start()
+t9.start()
+t10.start()
