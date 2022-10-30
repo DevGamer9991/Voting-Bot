6c45662cca9303d82b643b4ecea67fe5a992c542
Parker Thornton <pathornton114@gmail.com>
2022-10-29 22:12:21 -0500

    Added Main File

---
+++ vote.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+
+firstClr = False
+
+files = os.listdir("./configs")
+
+for configFile in files:
+    os.path.abspath(configFile)
+
+options = Options()
+
+options.add_argument('--log-level=3')
+options.add_argument("--window-size=1920,1080")
+options.add_argument('headless')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+counter = 1
+
+url = "https://okc.vypeok.com/2022/10/25/vote-now-okc-area-private-schools-miss-volleyball-presented-by-community-builders-poll-ends-11-1/"
+
+while True:
+    driver = webdriver.Chrome("C:\Program Files\Chrome Driver\chromedriver.exe", chrome_options=options, service_log_path='NULL')
+    
+    if (firstClr == False):
+        os.system("cls")
+        firstClr = True
+
+    print(f"\nOpening URL For the {counter} time.\n")
+    counter += 1
+    driver.get(url)
+
+    try: 
+        inputEl = driver.find_element(By.ID, "PDI_answer51314002")
+        voteButton = driver.find_element(By.ID, "pd-vote-button11227721")
+
+        inputEl.click()
+        voteButton.click()
+
+        voteTitle = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "question-top-11227721")))
+
+        voters = driver.find_elements(By.CLASS_NAME, "pds-feedback-group")
+
+        votes = 0
+        place = 1
+        for voter in voters:
+            name = voter.find_element(By.XPATH, "//label/span").text
+            if (name == "Natalie French – Christian Heritage"):
+
+                votes = voter.find_element(By.XPATH, "//label/span/span[@class='pds-feedback-votes']").text
+                votes = votes.split("(")[1]
+                votes = votes.split(")")[0]
+                votes = votes.split(" votes")[0]
+
+                placeString = ""
+                if (place == 1):
+                    placeString = "1st"
+                elif (place == 2):
+                    placeString = "2nd"
+                elif (place == 3):
+                    placeString = "3rd"
+                else:
+                    placeString = place + "th"
+
+                print(f"Natalie Has { votes } votes! And She is in { placeString } place!")
+                break
+            else:
+                place += 1
+
+        driver.close()
+    except:
+        print("Error Occured!")
+        driver.close()
+        continue
